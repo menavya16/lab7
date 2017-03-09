096f7a738ebaf7856fce920c13054a3339477422
menavya16 <sachdn4@mcmaster.ca>
2017-03-08 19:51:51 -0500

    Update BreadthFirstPaths.java

--- BreadthFirstPaths.java
+++ BreadthFirstPaths.java
@@ -5,9 +5,9 @@ import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
 
 public class BreadthFirstPaths {
-	private boolean[] marked;// Is a shortest path to vertex known?
-	private int[] edgeTo; // last vertex on known path to this vertex
-	private final int s; // source
+	private boolean[] marked;
+	private int[] edgeTo; 
+	private final int s; 
 
 	public BreadthFirstPaths(Graph G, int s) {
 		marked = new boolean[G.V()];
@@ -18,15 +18,15 @@ public class BreadthFirstPaths {
 
 	private void bfs(Graph G, int s) {
 		Queue<Integer> queue = new Queue<Integer>();
-		marked[s] = true; // Mark the source
-		queue.enqueue(s); // and put it on the queue.
+		marked[s] = true; 
+		queue.enqueue(s); 
 		while (!queue.isEmpty()) {
-			int v = queue.dequeue();// Remove next vertex frm queue
+			int v = queue.dequeue();
 			for (int w : G.adj(v))
-				if (!marked[w]) { // For every unmarked adjacent vertex,
-					edgeTo[w] = v;// save last edge on shortest path,
-					marked[w] = true;// mark it because path is known,
-					queue.enqueue(w);// and add it to the queue.
+				if (!marked[w]) { 
+					edgeTo[w] = v;
+					marked[w] = true;
+					queue.enqueue(w);
 				}
 		}
 	}
